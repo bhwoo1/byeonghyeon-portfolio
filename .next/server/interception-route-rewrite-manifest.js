c72ea120a29724f660970a6f72ca3813b558fd46
Byeonghyeon <problemp123@gmail.com>
2025-01-11 13:39:09 +0900

    [chore] gitignore

--- .next/server/interception-route-rewrite-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__INTERCEPTION_ROUTE_REWRITE_MANIFEST="[{\"source\":\"/project/:detail\",\"destination\":\"/project/(.):detail\",\"has\":[{\"type\":\"header\",\"key\":\"Next-Url\",\"value\":\"\\\\/project(?:\\\\/(.*))?[\\\\/#\\\\?]?\"}]}]"
