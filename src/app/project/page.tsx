c3d1eee5ee1a60ef6f29d47d454dd8678ec6aa9b
Byeonghyeon <problemp123@gmail.com>
2025-01-26 12:09:26 +0900

    [chore] test

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,72 +1,11 @@
-"use client";
-
-import React, { useEffect, useRef } from "react";
-import { useSearchParams } from "next/navigation";
-import ProjectList from "@/components/projcet/ProjectList";
-import ProjectPage from "@/components/projcet/ProjectPage";
-import RemoteControl from "@/components/RemoteControl";
-import { projectData } from "@/data/projectData";
+import PageContent from "@/components/projcet/PageContent";
+import React, { Suspense } from "react";
 
 const Page = () => {
-  const sectionRefs = useRef<(HTMLDivElement | null)[]>([]);
-  const projects = projectData;
-  const searchParams = useSearchParams();
-  const sectionQuery = searchParams.get("section");
-
-  const addSectionRef = (el: HTMLDivElement | null) => {
-    if (el && !sectionRefs.current.includes(el)) {
-      sectionRefs.current = [...sectionRefs.current, el];
-    }
-  };
-
-  const scrollToSection = (index: number) => {
-    const section = sectionRefs.current[index];
-    if (section) {
-      section.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-        inline: "start",
-      });
-    }
-  };
-
-  useEffect(() => {
-    if (sectionQuery) {
-      const sectionIndex = parseInt(sectionQuery, 10) - 1;
-      if (
-        !isNaN(sectionIndex) &&
-        sectionIndex >= 0 &&
-        sectionIndex < projects.length
-      ) {
-        scrollToSection(sectionIndex);
-      }
-    }
-  }, [sectionQuery, projects]);
-
   return (
-    <main className="w-full h-screen">
-      <section className="h-screen">
-        <ProjectList onScrollToSection={scrollToSection} />
-      </section>
-      {projects.map((project, index) => (
-        <section
-          key={project.name}
-          ref={addSectionRef}
-          className={`h-screen flex flex-col items-center justify-center ${
-            index === 0
-              ? "bg-blue-500"
-              : index === 1
-              ? "bg-violet-500"
-              : "bg-emerald-500"
-          }`}
-        >
-          <ProjectPage project={project} />
-        </section>
-      ))}
-      <div className="flex justify-center items-center">
-        <RemoteControl />
-      </div>
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <PageContent />
+    </Suspense>
   );
 };
 
